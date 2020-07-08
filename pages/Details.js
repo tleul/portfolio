446e590267dd012dc276e846fc00b97d5756f13e
MacUser <leult2018@gmail.com>
2020-07-07 22:06:16 -0700

    calnder chart

---
+++ pages/Details.js
@@ -0,0 +1,7 @@
+export default function details ()  {
+
+return (
+    <h1>hi</h1>
+)
+
+} 
--- pages/Navbar.js
+++ pages/Navbar.js
@@ -1,18 +1,19 @@
 
 import Link from 'next/link'
+import Router from 'next/router'
 import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
 const Gonavbar = (props) => {
      return(
          <>
 <Navbar  sticky="top" collapseOnSelect expand="lg" bg="light" >
-  <Navbar.Brand href="#home">
+  <Navbar.Brand  onClick={()=> Router.push('/', '/')}>
     <img width='200' height='100' src='/logo.png'></img>
   </Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
       <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="/Covid">Covid</Nav.Link>
+      <Nav.Link onClick={()=> Router.push('/Covid', '/index' , {shallow: true})}> Covid  </Nav.Link>
       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
