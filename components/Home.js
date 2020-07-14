0d5687b8b044df6b182137639f0bd80db2a0da98
Tekleselassie Leul <edenlove2019@gmail.com>
2020-07-13 20:32:18 -0700

    pf

--- components/Home.js
+++ components/Home.js
@@ -1,21 +1,46 @@
 import SkillShuffler from './Titlehome';
+import CovidTbleTwo from './CovidTbleTwo';
+import { connect } from 'react-redux';
+import { getCovidData } from './../store/actions/index';
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const Home = () => {
+const Home = ({ getCovidData, loading }) => {
+	setInterval(() => {
+		getCovidData();
+	}, 30000);
+	useEffect(() => {
+		getCovidData();
+	}, []);
 	return (
-		<div className='protoflio-home'>
-			<div className='top-title'>
-				<h3>
-					Welcome In!!
-					<br />
-					This is Leul, T
-					<br />I am Fullstack software Developer!
-				</h3>
-			</div>
+		<>
+			<div className='main-container'>
+				<div className='protoflio-home'>
+					<div className='top-title'>
+						<h3>
+							Welcome In!!
+							<br />
+							This is Leul, T
+							<br />I am Fullstack software Developer!
+						</h3>
+					</div>
 
-			<div className='shuffle'>
-				<SkillShuffler />
+					<div className='shuffle'>
+						<SkillShuffler />
+					</div>
+				</div>
+				<div className='covid-showroom'>
+					<CovidTbleTwo />
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
-export default Home;
+Home.propTypes = {
+	getCovidData: PropTypes.func.isRequired,
+	loading: PropTypes.bool.isRequired,
+};
+const mapStateToProps = (state) => ({
+	loading: state.covid.loading,
+});
+export default connect(mapStateToProps, { getCovidData })(Home);
