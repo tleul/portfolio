1cfe482af86afe8e447f54c4cad257caf8af768f
tleul <leult2018@gmail.com>
2020-10-23 20:31:41 -0700

    SEO meta tag added

--- components/Wrapper.js
+++ components/Wrapper.js
@@ -1,33 +1,44 @@
-import Head from "next/head";
-import { Fragment } from "react";
+import Head from 'next/head';
+import { Fragment } from 'react';
 // import { ToastContainer } from "react-toastify";
 
 const Wrapper = ({ children }) => {
-  return (
-    <Fragment>
-      <Head>
-        <title>Tafach Tech</title>
-        <link
-          rel="stylesheet"
-          href="https://bootswatch.com/4/cerulean/bootstrap.min.css"
-        />
-        <script src="https://kit.fontawesome.com/db873bbd1d.js" crossOrigin="anonymous"></script>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-      
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-       
-      </Head>
+	return (
+		<Fragment>
+			<Head>
+				<title>Tafach Tech</title>
+				<meta
+					name='google-site-verification'
+					content='sK7k4q13AvLy0XoCM2EiKPsNTsvLO68sAvOv8w-IOoY'
+				/>
+				<link
+					rel='stylesheet'
+					href='https://bootswatch.com/4/cerulean/bootstrap.min.css'
+				/>
+				<script
+					src='https://kit.fontawesome.com/db873bbd1d.js'
+					crossOrigin='anonymous'></script>
+				<link
+					rel='stylesheet'
+					href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+				/>
 
-      {/* <ToastContainer
+				<link
+					rel='stylesheet'
+					href='https://fonts.googleapis.com/icon?family=Material+Icons'
+				/>
+			</Head>
+
+			{/* <ToastContainer
         className="alerts"
         bodyClassName="alertb"
         progressClassName="alertp"
         draggable={false}
       /> */}
 
-      <div className="mcontainer">{children}</div>
-    </Fragment>
-  );
+			<div className='mcontainer'>{children}</div>
+		</Fragment>
+	);
 };
 
 export default Wrapper;
